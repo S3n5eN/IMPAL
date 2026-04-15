1cd7bec76dfce9b1cbd98d7841f3256da3df78ca
Glen <glendavid344@gmail.com>
2026-04-15 21:02:07 +0700

    Api Login

---
+++ app/api/register/login/route.ts
@@ -0,0 +1,33 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
+const jwtsecret = process.env.JWT_SECRET
+
+export async function POST(req: Request) {
+  const {email,password} = await req.json();
+
+  const user = await prisma.user.findUnique({
+    where: {email},
+  });
+
+  if (!user) {
+    return NextResponse.json({ error: "User tidak ditemukan" }, { status: 404 });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return NextResponse.json({ error: "Password salah" }, { status: 401 });
+  }
+  const token = jwt.sign({user.id}, jwtsecret, {expiresIn: '2h'})
+  return NextResponse.json({
+    message: "Login berhasil",
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    },
+    token
+  });
+}
